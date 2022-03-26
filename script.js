251d43e56717a25eac8382a7dc5a2c2b0eea262d
Eduardo Carreiro <eduardo@wpcwebsolutions.com>
2022-03-25 21:00:41 -0300

    create 2nd way to solve the same problem

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const fs = require("fs/promises");
 
+// ==== 1a. Way to Do (Object Oriented Programming) ====
+
 const sum = (a, b) => {
 	return new Promise(async (resolve, reject) => {
 		if (typeof (a + b) !== "number") {
-			return resolve("NaN");
+			return resolve(console.log("NaN"));
 		}
 		const sumTotal = a + b;
 		try {
@@ -15,7 +17,28 @@ const sum = (a, b) => {
 	});
 };
 
+
+// ======== 2a. Way to Do (only async await)========
+
+const sum1 = async (a, b) => {
+	const sumTotal = a + b;
+	if (typeof sumTotal !== "number") {
+		return console.log("NaN");
+	}
+	try {
+		await fs.appendFile("resultado.txt", `${a} + ${b} = ${sumTotal}\n`);
+		console.log("Arquivo gravado com sucesso");
+	} catch {
+		console.log("problema na gravação do arquivo");
+	}
+};
+
 sum(10, 5);
 sum(8, 26);
-console.log(sum(4, "a"));
-console.log(sum("3", 2));
+sum(4, "a");
+sum("3", 2);
+
+sum1(10, 5));
+sum1(8, 26));
+sum1(4, "a");
+sum1("3", 2);
